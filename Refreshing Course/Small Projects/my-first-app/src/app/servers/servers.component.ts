33b6fc1d63c8ef7e7099f705ba0f5885bb4bbba9
Pedro López <plopez@financialforce.com>
2023-03-02 20:40:50 +0100

    Lesson 35

--- Refreshing Course/Small Projects/my-first-app/src/app/servers/servers.component.ts
+++ Refreshing Course/Small Projects/my-first-app/src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ export class ServersComponent {
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server was created';
   serverName: string = '';
+  username: string = '';
 
   constructor() {
     setTimeout(() => {
@@ -15,11 +16,15 @@ export class ServersComponent {
     }, 3000);
   }
 
-  onCreateServer() {
+  onCreateServer(): void {
     this.serverCreationStatus = 'A server was created, name is: ' + this.serverName;
   }
 
-  onInputChange(event: any) {
+  onInputChange(event: any): void {
     this.serverName = event.target.value;
+  }
+
+  onResetUsername(): void {
+    this.username = '';
   }
 }
