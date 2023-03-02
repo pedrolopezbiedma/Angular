687d66182c6d382b4f497e9772f7005fa0591a31
Pedro López <plopez@financialforce.com>
2023-03-02 20:48:33 +0100

    Lesson 37

--- Refreshing Course/Small Projects/my-first-app/src/app/servers/servers.component.ts
+++ Refreshing Course/Small Projects/my-first-app/src/app/servers/servers.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 export class ServersComponent {
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server was created';
+  serverCreated: boolean = false;
   serverName: string = '';
   username: string = '';
 
@@ -18,6 +19,7 @@ export class ServersComponent {
 
   onCreateServer(): void {
     this.serverCreationStatus = 'A server was created, name is: ' + this.serverName;
+    this.serverCreated = true;
   }
 
   onInputChange(event: any): void {
