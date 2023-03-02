a73a33e295a2cbbb201e31c4798676d9c850bac3
Pedro López <plopez@financialforce.com>
2023-03-02 20:28:35 +0100

    Lesson 33

--- Refreshing Course/Small Projects/my-first-app/src/app/app.module.ts
+++ Refreshing Course/Small Projects/my-first-app/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { SuccessAlertComponent } from './success-alert/success-alert.comopnent';
     SuccessAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
