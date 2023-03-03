9b65e659ec0679d15d331bbec1194218f96c2336
Pedro López <plopez@financialforce.com>
2023-03-03 23:28:07 +0100

    Lesson 60

--- Refreshing Course/Course Project/recipe-book/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ Refreshing Course/Course Project/recipe-book/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,8 @@
+// Angular
 import { Component } from '@angular/core';
+
+// Component & Model
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +10,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes:Recipe[] = [
+    new Recipe('Test Recipe 1 Name', 'Test Recipe 1 Description', 'https://i.blogs.es/173514/croquetas/450_1000.jpeg'),
+    new Recipe('Test Recipe 2 Name', 'Test Recipe 2 Description', 'https://i.blogs.es/173514/croquetas/450_1000.jpeg')
+  ];
 }
