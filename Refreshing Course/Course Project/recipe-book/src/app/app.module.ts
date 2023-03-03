54ea6f44b19d5bee01e275cde63614c11f403182
Pedro López <plopez@financialforce.com>
2023-03-03 16:48:56 +0100

    Lesson 49

--- Refreshing Course/Course Project/recipe-book/src/app/app.module.ts
+++ Refreshing Course/Course Project/recipe-book/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailsComponent } from './recipe-book/recipe-details/recipe-details.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { NewIngredientComponent } from './shopping-list/new-ingredient/new-ingredient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailsComponent,
+    ShoppingListComponent,
+    NewIngredientComponent
   ],
   imports: [
     BrowserModule
