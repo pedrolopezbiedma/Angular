41baaf27503d0ce7169de432c48363b7001edf11
Pedro López <plopez@financialforce.com>
2023-03-04 16:09:15 +0100

    Lesson 93

--- Refreshing Course/Small Projects/directives/src/app/app.module.ts
+++ Refreshing Course/Small Projects/directives/src/app/app.module.ts
@@ -1,13 +1,18 @@
+// Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+// Directives
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 
+// Components & Models
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule,
