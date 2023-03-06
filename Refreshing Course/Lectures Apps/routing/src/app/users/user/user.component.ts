4c368eb03d8c5eeab4296ec7f1b1eb09d7b72419
Pedro López <plopez@financialforce.com>
2023-03-06 23:11:52 +0100

    Lesson 135

--- Refreshing Course/Lectures Apps/routing/src/app/users/user/user.component.ts
+++ Refreshing Course/Lectures Apps/routing/src/app/users/user/user.component.ts
@@ -1,4 +1,7 @@
+// Angular
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -8,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    }
+
+    this.route.params.subscribe((params:Observable<Params>) => {
+      this.user = {
+        id: params['id'],
+        name: params['name']
+      }
+    })
   }
 
 }
