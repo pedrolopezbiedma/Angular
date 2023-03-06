1eb840a337adeb5c52754f6d737022784324c7fa
Pedro López <plopez@financialforce.com>
2023-03-06 22:41:52 +0100

    Lesson 130

--- Refreshing Course/Lectures Apps/routing/src/app/home/home.component.ts
+++ Refreshing Course/Lectures Apps/routing/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
+// Angular
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onLoadServers() : void {
+    this.router.navigate(['/servers']);
   }
 
 }
