359f8c962b41e2582f694c07cbab5449fc8e6840
Pedro López <plopez@financialforce.com>
2023-03-08 18:22:33 +0100

    Lesson 172

--- Refreshing Course/Lectures Apps/observables/src/app/home/home.component.ts
+++ Refreshing Course/Lectures Apps/observables/src/app/home/home.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  firstObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    this.firstObsSubscription = interval(1000).subscribe((count: number) => {
+      console.log('count -->', count)
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
   }
 
 }
