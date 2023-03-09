b33c5d325887f7e1811c1ca45cea5c3e81310828
Pedro López <plopez@financialforce.com>
2023-03-09 11:34:03 +0100

    Lesson 208

--- Refreshing Course/Lectures Apps/forms-reactive/src/app/app.component.ts
+++ Refreshing Course/Lectures Apps/forms-reactive/src/app/app.component.ts
@@ -16,8 +16,10 @@ export class AppComponent implements OnInit {
 
   initializeForm(): void {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null, [Validators.required]),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'userData': new FormGroup({
+        'username': new FormControl(null, [Validators.required]),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
       'gender': new FormControl('male'),
     });
   }
