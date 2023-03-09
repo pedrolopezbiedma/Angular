e4aeca101e6fee5dd2d8bc09d7bc0d1752310f00
Pedro López <plopez@financialforce.com>
2023-03-09 11:59:43 +0100

    Lesson 215

--- Refreshing Course/Lectures Apps/forms-reactive/src/app/app.component.ts
+++ Refreshing Course/Lectures Apps/forms-reactive/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,16 +13,34 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.initializeForm();
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Amna'
+      }
+    })
   }
 
   initializeForm(): void {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required]),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmail)
       }),
       'gender': new FormControl('male'),
     });
+  }
+
+  forbiddenEmail(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if(control.value === 'test@test.com'){
+          resolve({ emailIsForbidden: true});
+        } else {
+          resolve(null);
+        }
+      }, 1500)
+    })
+    return promise;
   }
 
   onSubmit(): void {
